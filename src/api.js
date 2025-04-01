dfca385f06cbe1064571b7c01032e4534c6daba4
Amada García <a01569104@tec.mx>
2025-04-01 16:37:14 +0000

    upd

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const getUsuario = async (user_id) => {
 
-    const response = await axios.get('http://localhost:2025/test/' + user_id, {
+    const response = await axios.get('https://bookish-winner-7vrv5rv94qqx3xqgp-2025.app.github.dev/test/' + user_id, {
     });
     
     console.log(response)
